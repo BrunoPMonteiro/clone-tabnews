f7102f97eeb536c26001af774610eb3db92f9035
Bruno Monteiro <93095745+BrunoPMonteiro@users.noreply.github.com>
2026-02-11 14:13:28 +0000

    Mais uma parte da aula 20

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,11 +5,28 @@ async function status(request, response) {
 
   const dataBaseVersionResult = await database.query("SHOW server_version;");
   const dataBaseVersionValue = dataBaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(`
+  SELECT setting AS max_connections
+  FROM pg_settings
+  WHERE name = 'max_connections';
+`);
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].opened_connections;
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: dataBaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: Number(databaseOpenedConnectionsValue),
       },
     },
   });
